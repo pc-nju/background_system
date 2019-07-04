6d9389a6c875c2ca3f81553632fb448695296803
pc <744763941@qq.com>
2019-07-04 19:13:58 +0800

    删除无效测试

--- src/test/java/com/bms/BmsApplicationTests.java
+++ src/test/java/com/bms/BmsApplicationTests.java
@@ -13,53 +13,8 @@ import java.util.List;
 @SpringBootTest
 public class BmsApplicationTests {
 
-	class Animal {
-		private String type;
-		private int age;
-
-		public String getType() {
-			return type;
-		}
-
-		public void setType(String type) {
-			this.type = type;
-		}
-
-		public int getAge() {
-			return age;
-		}
-
-		public void setAge(int age) {
-			this.age = age;
-		}
-
-		public Animal(String type, int age) {
-			this.type = type;
-			this.age = age;
-		}
-
-		@Override
-		public String toString() {
-			return "Animal{" +
-					"type='" + type + '\'' +
-					", age=" + age +
-					'}';
-		}
-	}
-
 	@Test
 	public void contextLoads() {
-		Animal animal1 = new Animal("dog", 1);
-		Animal animal2 = new Animal("cat", 1);
-		Animal animal3 = new Animal("bird", 1);
-		List<Animal> animals = new ArrayList<>();
-		animals.add(animal1);
-		animals.add(animal2);
-		animals.add(animal3);
-		animals.forEach(animal -> {
-			animal.setAge(2);
-		});
-		System.out.println(animals);
 	}
 
 }
